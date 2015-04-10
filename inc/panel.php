e9d5ac89aa00339a3e949c0efc91430df583c36d
Burak Varli <burakvarli@yandex.com>
2015-04-10 11:59:21 +0300

    panel'deki kutulara işlev kazandırıldı

--- inc/panel.php
+++ inc/panel.php
@@ -2,14 +2,18 @@
 
 class Lechuguilla_Ayar {
 
-    private $options;
+    public $liste_tipleri = array(
+        'yeni-filmler' => 'Yeni Filmler',
+        'populer-filmler' => 'Popüler Filmler',
+        'kategori' => 'Kategori'
+    );
 
-    public function __construct() {
+    function __construct() {
         add_action('admin_menu', array($this, 'add_plugin_page'));
         add_action('admin_init', array($this, 'page_init'));
     }
 
-    public function add_plugin_page() {
+    function add_plugin_page() {
         $hook = add_menu_page(
             'Lechuguilla Ayar Sayfası',
             'Lechuguilla',
@@ -34,65 +38,58 @@ class Lechuguilla_Ayar {
         });
     }
 
-    public function lechuguilla_ayar_sayfasi() {
-        // Set class property
-        $this->options = get_option( 'my_option_name' );
-        var_dump($this->options);
-        
-        ?>
+    function page_init() {
+        register_setting('lechuguilla-ayar-grup', 'liste-tip');        
+        register_setting('lechuguilla-ayar-grup', 'liste-sayi');        
+    }
+
+    function lechuguilla_ayar_sayfasi() { ?>
+
         <div id="lechuguilla_ayar" class="wrap">
             <h2>Lechuguilla Ayar Sayfası</h2>           
             <form method="post" action="options.php">
-            
+            <?php 
+                settings_fields('lechuguilla-ayar-grup'); 
+                do_settings_sections('lechuguilla-ayar-grup');
+
+                $tipler = get_option('liste-tip');
+                $sayilar = get_option('liste-sayi');
+            ?>
             <h3>Anasayfa</h3>
 
             <div id="anasayfa">
-                <div class="liste">
-                    <div class="liste-ust">
-                        Yeni Filmler
-                        <div class="liste-toggle"></div>
-                    </div>
-                    <div class="liste-icerik">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur nobis repellat nostrum, voluptate, esse rerum omnis, doloremque ratione, doloribus error. Corporis sequi excepturi sed repellat eum dolorem eveniet, hic.
-                    </div>
-                </div>
 
-                <div class="liste">
-                    <div class="liste-ust">
-                        Yeni Filmler
-                        <div class="liste-toggle"></div>
+                <?php foreach ($tipler as $sira => $tip) : ?>
+                    <div class="liste">
+                        <div class="liste-ust">
+                            <?php echo $this->liste_tipleri[$tip] ?>
+                            <div class="liste-toggle"></div>
+                        </div>
+                        <div class="liste-icerik">
+                            <div>
+                            <label>
+                                Liste tipi
+                                <select name="liste-tip[]">
+                                    <?php foreach ($this->liste_tipleri as $key => $value) {
+                                        echo '<option '.($key == $tip ? 'selected' : '').' value="'.$key.'">'.$value.'</option>';
+                                    } ?>
+                                </select>
+                            </label>
+                            </div>
+                            
+                            <div>
+                            <label>
+                                Kaç film gösterilsin
+                                <input type="text" name="liste-sayi[]" value="<?php echo $sayilar[$sira]; ?>">
+                            </label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="liste-icerik">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur nobis repellat nostrum, voluptate, esse rerum omnis, doloremque ratione, doloribus error. Corporis sequi excepturi sed repellat eum dolorem eveniet, hic.
-                    </div>
-                </div>
-
-                <div class="liste">
-                    <div class="liste-ust">
-                        Yeni Filmler
-                        <div class="liste-toggle"></div>
-                    </div>
-                    <div class="liste-icerik">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur nobis repellat nostrum, voluptate, esse rerum omnis, doloremque ratione, doloribus error. Corporis sequi excepturi sed repellat eum dolorem eveniet, hic.
-                    </div>
-                </div>
-
-                <div class="liste">
-                    <div class="liste-ust">
-                        Yeni Filmler
-                        <div class="liste-toggle"></div>
-                    </div>
-                    <div class="liste-icerik">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur nobis repellat nostrum, voluptate, esse rerum omnis, doloremque ratione, doloribus error. Corporis sequi excepturi sed repellat eum dolorem eveniet, hic.
-                    </div>
-                </div>
+                <?php endforeach; ?>
+       
             </div>
 
-
             <?php
-                // This prints out all hidden setting fields
-                settings_fields('my_option_group');   
-                do_settings_sections('my-setting-admin');
                 submit_button('Kaydet'); 
             ?>
             </form>
@@ -100,81 +97,5 @@ class Lechuguilla_Ayar {
         <?php
     }
 
-    public function page_init() {
-        register_setting(
-            'my_option_group', // Option group
-            'my_option_name', // Option name
-            array( $this, 'sanitize' ) // Sanitize
-        );
-
-        add_settings_section(
-            'setting_section_id', // ID
-            'My Custom Settings', // Title
-            array( $this, 'print_section_info' ), // Callback
-            'my-setting-admin' // Page
-        );  
-
-        add_settings_field(
-            'id_number', // ID
-            'ID Number', // Title 
-            array( $this, 'id_number_callback' ), // Callback
-            'my-setting-admin', // Page
-            'setting_section_id' // Section           
-        );      
-
-        add_settings_field(
-            'title', 
-            'Title', 
-            array( $this, 'title_callback' ), 
-            'my-setting-admin', 
-            'setting_section_id'
-        );
-    }
-
-    /**
-     * Sanitize each setting field as needed
-     *
-     * @param array $input Contains all settings fields as array keys
-     */
-    public function sanitize( $input )
-    {
-        $new_input = array();
-        if( isset( $input['id_number'] ) )
-            $new_input['id_number'] = absint( $input['id_number'] );
-
-        if( isset( $input['title'] ) )
-            $new_input['title'] = sanitize_text_field( $input['title'] );
-
-        return $new_input;
-    }
-
-    /** 
-     * Print the Section text
-     */
-    public function print_section_info()
-    {
-        print 'Enter your settings below:';
-    }
-
-    /** 
-     * Get the settings option array and print one of its values
-     */
-    public function id_number_callback()
-    {
-        printf(
-            '<input type="text" id="id_number" name="my_option_name[id_number]" value="%s" />',
-            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
-        );
-    }
-
-    /** 
-     * Get the settings option array and print one of its values
-     */
-    public function title_callback()
-    {
-        printf(
-            '<input type="text" id="title" name="my_option_name[title]" value="%s" />',
-            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
-        );
-    }
+    
 }
--- scripts/panel.js
+++ scripts/panel.js
@@ -13,15 +13,4 @@ jQuery(document).ready(function() {
     jQuery(".liste-toggle").click(function() {
         jQuery(this).closest(".liste").toggleClass('acik');
     });
-    
-   /* 
-    jQuery("#save-order").bind( "click", function() {
-        
-        jQuery("html, body").animate({ scrollTop: 0 }, "fast");
-        
-        jQuery.post( ajaxurl, { action:'update-custom-type-order', order:jQuery("#sortable").sortable("serialize") }, function() {
-            jQuery("#ajax-response").html('<div class="message updated fade"><p><?php _e('Items Order Updated', 'cpt') ?></p></div>');
-            jQuery("#ajax-response div").delay(3000).hide("slow");
-        });
-    });*/
 });
